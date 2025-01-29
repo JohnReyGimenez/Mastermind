a8442e63aa4afa3a2c8d0589b4e650b7300c45a1
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-29 21:40:03 +0800

    fixed indentation and removed ends

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
+
 Class Board
-  COLORS = {
-    1 => " 1 ".colorize(:red),
-    2 => " 2 ".colorize(:blue),
-    3 => " 3 ".colorize(:green),
-    4 => " 4 ".colorize(:yellow),
-    5 => " 5 ".colorize(:magenta),
-    6 => " 6 ".colorize(:cyan),
-    }
+COLORS = {
+  1 => ' 1 '.colorize(:red),
+  2 => ' 2 '.colorize(:blue),
+  3 => ' 3 '.colorize(:green),
+  4 => ' 4 '.colorize(:yellow),
+  5 => ' 5 '.colorize(:magenta),
+  6 => ' 6 '.colorize(:cyan)
+}.freeze
 
-  def initialize
-    @board = Array.new(8, " ") #create a board with 8 nil elements
-  end
+def initialize
+  @board = Array.new(8, ' ') # create a board with 8 nil elements
+end
 
-  def display_board()
-    puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
-    puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
-    puts "-----------"
-    puts "Feedback: #{board[4]} #{board[5]} #{board[6]} #{board[7]}"
-  end
-
-  
+def display_board
+  puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
+  puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
+  puts '-----------'
+  puts "Feedback: #{board[4]} #{board[5]} #{board[6]} #{board[7]}"
 end
