92135b4008ab377d46543530c1d493f4ff5f2c5d
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-29 19:39:48 +0800

    initialized board

--- lib/Mastermind/board.rb
+++ lib/Mastermind/board.rb
@@ -1,7 +1,13 @@
 Class Board
-  def 
+  def initialize
+    @board = Array.new(8, " ") #create a board with 8 nil elements
+  end
 
+  def display_board()
+    puts " #{board[0]} #{board[0]}  ##{board[1]} ##{board[1]}  ##{board[2]} ##{board[2]}  ##{board[3]} ##{board[0]} ##{board[0]} ##{board[0]}"
+    puts " #{board[0]} #{board[0]}}  ##{board[1]} ##{board[1]} ##{board[2]} ##{board[2]} ##{board[3]} ##{board[0]} ##{board[0]} ##{board[0]}"
 
+  end
 
 
 end
