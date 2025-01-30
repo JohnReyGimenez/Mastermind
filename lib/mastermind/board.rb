bf82ec30cd489572e07eae427323025decfc15cc
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-30 21:27:09 +0800

    fixed display board and class indentation

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
 
-Class Board
-COLORS = {
-  1 => ' 1 '.colorize(:red),
-  2 => ' 2 '.colorize(:blue),
-  3 => ' 3 '.colorize(:green),
-  4 => ' 4 '.colorize(:yellow),
-  5 => ' 5 '.colorize(:magenta),
-  6 => ' 6 '.colorize(:cyan)
-}.freeze
+class Board
+  COLORS = {
+    1 => ' 1 '.colorize(:red),
+    2 => ' 2 '.colorize(:blue),
+    3 => ' 3 '.colorize(:green),
+    4 => ' 4 '.colorize(:yellow),
+    5 => ' 5 '.colorize(:magenta),
+    6 => ' 6 '.colorize(:cyan)
+  }.freeze
 
-def initialize
-  @board = Array.new(8, ' ') # create a board with 8 nil elements
-end
+  def initialize
+    @board = Array.new(8, ' ') # create a board with 8 nil elements
+  end
 
-def display_board
-  puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
-  puts " #{board[0]} #{board[0]}  #{board[1]} #{board[1]} #{board[2]} #{board[2]} #{board[3]} #{board[3]} "
-  puts '-----------'
-  puts "Feedback: #{board[4]} #{board[5]} #{board[6]} #{board[7]}"
-end
+  def display_board
+    puts " #{COLORS[@board[0]]} #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[1]]}
+     #{COLORS[@board[2]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]} #{COLORS[@board[3]]}"
 
-def update_cell(index, _value)
-  @board[index] = value
+    puts " #{COLORS[@board[0]]} #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[1]]}
+     #{COLORS[@board[2]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]} #{COLORS[@board[3]]}"
+
+    puts '-----------'
+    puts "Feedback: #{board[4]} #{board[5]} #{board[6]} #{board[7]}"
+  end
+
+  def update_cell(index, _value)
+    @board[index] = value
+  end
 end
