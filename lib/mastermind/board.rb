ac8fb063e89ad9b508cd497c884d44449419b747
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-30 22:07:05 +0800

    added feedback in board

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -8,8 +8,10 @@ class Board
     2 => '  '.colorize(background: :blue),
     3 => '  '.colorize(background: :green),
     4 => '  '.colorize(background: :yellow),
-    5 => '  '.colorize(background: :magenta),
-    6 => '  '.colorize(background: :cyan)
+    5 => '  '.colorize(background: :white),
+    6 => '  '.colorize(background: :black),
+    7 => '  '.colorize(background: :white),
+    8 => '  '.colorize(background: :black)
   }.freeze
 
   def initialize
@@ -19,7 +21,7 @@ class Board
   def display_board
     puts " #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]}"
     puts ' -----------'
-    puts "Feedback: #{@board[4]} #{@board[5]} #{@board[6]} #{@board[7]}"
+    puts "Feedback: #{COLORS[@board[4]]} #{COLORS[@board[5]]} #{COLORS[@board[6]]} #{COLORS[@board[7]]}"
   end
 
   def update_cell(index, value)
@@ -33,5 +35,9 @@ board.update_cell(0, 1) # Red
 board.update_cell(1, 2) # Blue
 board.update_cell(2, 3) # Green
 board.update_cell(3, 4) # Yellow
+board.update_cell(4, 5)
+board.update_cell(5, 6)
+board.update_cell(6, 7)
+board.update_cell(7, 8)
 
 board.display_board
