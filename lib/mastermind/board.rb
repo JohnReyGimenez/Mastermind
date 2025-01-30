de9ed92e5510f26b3fa590a8f4f5273df1a075b4
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-30 21:55:56 +0800

    fixed and finalized the code and colors

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 class Board
   COLORS = {
-    1 => ' 1 '.colorize(:red),
-    2 => ' 2 '.colorize(:blue),
-    3 => ' 3 '.colorize(:green),
-    4 => ' 4 '.colorize(:yellow),
-    5 => ' 5 '.colorize(:magenta),
-    6 => ' 6 '.colorize(:cyan)
+    1 => '  '.colorize(background: :red),
+    2 => '  '.colorize(background: :blue),
+    3 => '  '.colorize(background: :green),
+    4 => '  '.colorize(background: :yellow),
+    5 => '  '.colorize(background: :magenta),
+    6 => '  '.colorize(background: :cyan)
   }.freeze
 
   def initialize
@@ -15,17 +17,21 @@ class Board
   end
 
   def display_board
-    puts " #{COLORS[@board[0]]} #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[1]]}
-     #{COLORS[@board[2]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]} #{COLORS[@board[3]]}"
-
-    puts " #{COLORS[@board[0]]} #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[1]]}
-     #{COLORS[@board[2]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]} #{COLORS[@board[3]]}"
-
-    puts '-----------'
-    puts "Feedback: #{board[4]} #{board[5]} #{board[6]} #{board[7]}"
+    puts " #{COLORS[@board[0]]} #{COLORS[@board[1]]} #{COLORS[@board[2]]} #{COLORS[@board[3]]}"
+    puts ' -----------'
+    puts "Feedback: #{@board[4]} #{@board[5]} #{@board[6]} #{@board[7]}"
   end
 
-  def update_cell(index, _value)
+  def update_cell(index, value)
     @board[index] = value
   end
 end
+
+board = Board.new
+
+board.update_cell(0, 1) # Red
+board.update_cell(1, 2) # Blue
+board.update_cell(2, 3) # Green
+board.update_cell(3, 4) # Yellow
+
+board.display_board
