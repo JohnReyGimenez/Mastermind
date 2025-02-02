01595a5d071be16e5c99d787ea6a8413726ce331
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-02 22:07:08 +0800

    added basic draft for loop in playgame

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 class Game
   def input_to_index(user_input)
     user_input.to_i - 1
@@ -7,6 +9,14 @@ class Game
 
   def play_game
     loop do
+      board.display_board
+      puts "input your code
+          1: #{'Red'.colorize(:red)}
+          2: #{'Blue'.colorize(:blue)}
+          3: #{'Green'.colorize(:green)}
+          4: #{'Yellow'.colorize(:yellow)}
+          5: #{'Magenta'.colorize(:magenta)}
+          6: #{'Cyan'.colorize(:cyan)}"
     end
   end
 end
