12b44ff563ff6d92f200475e6d88458d373ce382
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-05 21:57:27 +0800

    added secret code variable

--- lib/mastermind/code_guesser.rb
+++ lib/mastermind/code_guesser.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class CodeGuesser
+  secret_code = Random.new.rand(1..6)
 end
+
+# function - if player has chosen code guesser return true
+# codeguesser game function - main loop for code guesser
