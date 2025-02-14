d0eb06644a25e21a9f8bb6fbe89c88a4247dfd37
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-14 19:52:09 +0800

    completed initialize method

--- lib/mastermind/code_maker.rb
+++ lib/mastermind/code_maker.rb
@@ -3,5 +3,20 @@
 class CodeMaker
   def initialize
     @board = board
+    @attempts = 10
+    @previous_guesses = []
+    @possible_codes = (1..6).to_a.repeated_permutation(4).to_a # array of all possible 4 digit codes
+    # from num 1 to 6
+  end
+
+  def play_as_maker
+    @attempts = 10
+  end
+
+  def get_player_feedback
   end
 end
+
+#
+#
+#
