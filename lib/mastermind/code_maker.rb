1dd706bfe2af4c65520b86b1258197889c3909a3
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-14 20:22:03 +0800

    initialized draft for play as maker

--- lib/mastermind/code_maker.rb
+++ lib/mastermind/code_maker.rb
@@ -11,9 +11,27 @@ class CodeMaker
 
   def play_as_maker
     @attempts = 10
+    secret_code = get_player_code
+
+    while attempts.positive?
+      @board.display_board
+      random_guess while attempts > 5
+    end
+    @attempts -= 1
+  end
+
+  def random_guess
+    [*1..6].sample
+  end
+
+  def refined_guess
   end
 
   def get_player_feedback
+  end
+
+  def get_player_code
+    gets.chomp.to_a
   end
 end
 
