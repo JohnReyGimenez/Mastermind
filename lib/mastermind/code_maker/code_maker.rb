98bee060f3588beb4e95de5de4a85e0732ea9559
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-17 19:44:19 +0800

    seperated play as maker to two functions

--- lib/mastermind/code_maker/code_maker.rb
+++ lib/mastermind/code_maker/code_maker.rb
@@ -7,30 +7,17 @@ class CodeMaker
     @previous_guesses = []
   end
 
-  def play_as_maker(difficulty = :medium)
+  def play_as_maker
     difficulty = PlayerInput.choose_difficulty
     secret_code = PlayerInput.get_player_code
     @board.set_secret_code(secret_code) # Set the secret code on the board
     guesser = ComputerGuesser.new(difficulty)
 
     while @attempts.positive?
-      guess = if @previous_guesses.size < random_guesses
-                random_guess
-              else
-                refine_based_on_feedback(@previous_guesses.last[:feedback])
-              end
-      puts "Computers guess: #{guess.join(' ')}"
-      feedback = get_player_feedback(guess)
-      @previous_guesses << { guess: guess, feedback: feedback }
+      play_turn(guesser, secret_code)
 
-      # updates the board
-      guess.each_with_index { |value, index| @board.update_cell(index, value) }
-      feedback.each_with_index { |value, index| @board.update_cell(index, value) }
-
-      clear_screen
-      @board.display_board(show_secret_code: true)
-
-      if won?(secret_code, guess)
+      # checks if computer correctly guessed the code
+      if won?(secret_code, previous_guesses.last[:feedback])
         puts "the computer guess your code! #{guess.join(' ')}"
         return
       end
@@ -39,6 +26,22 @@ class CodeMaker
       puts "Remaining attempts: #{@attempts}"
     end
     puts 'the computer has failed in guessing your secret code. You win!!'
+  end
+
+  def play_turn(guesser, secret_code)
+    # generate computers next code
+    guess = guesser.next_guess(@previous_guesses)
+    puts "Computers guess: #{guess.join(' ')}"
+
+    # gets feedback from player
+    feedback = get_player_feedback(guess)
+    @previous_guesses << { guess: guess, feedback: feedback }
+
+    # updates the board
+    guess.each_with_index { |value, index| @board.update_cell(index, value) }
+    feedback.each_with_index { |value, index| @board.update_cell(index, value) }
+
+    @board.display_board(show_secret_code: true)
   end
 
   def won?(secret_code, guess)
