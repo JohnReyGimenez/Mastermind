6e0592e6c9524d41918fdec7640530c408ee411a
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-21 22:58:50 +0800

    optimized refine based on feedback function

--- lib/mastermind/game_logic.rb
+++ lib/mastermind/game_logic.rb
@@ -9,26 +9,13 @@ module GameLogic
       if peg == :black
         next
       elsif peg == :white
-
-      end
-    end
-
-    if feedback[0..1].any? { |peg| %i[black white].include?(peg) }
-      new_guess[0] = rand(1..6).to_a.reject { |n| n == last_guess[0] }.sample unless feedback[0] == :black
-      new_guess[1] = rand(1..6).to_a.reject { |n| n == last_guess[1] }.sample unless feedback[1] == :black
-    end
-
-    if feedback[2..3].any? { |peg| %i[black white].include?(peg) }
-      new_guess[2] = rand(1..6).to_a.reject { |n| n == last_guess[2] }.sample unless feedback[2] == :black
-      new_guess[3] = rand(1..6).to_a.reject { |n| n == last_guess[3] }.sample unless feedback[3] == :black
-    end
-
-    feedback.each_with_index do |peg, i|
-      if peg == :white
-        swap_index = (i + 1) % 4
+        swap_index = find_swap_index(new_guess, i)
         new_guess[i], new_guess[swap_index] = new_guess[swap_index], new_guess[i]
+      else
+        new_guess[i] = generate_new_color(new_guess, i)
       end
     end
+
     new_guess
   end
 
