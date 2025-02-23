1ca304da1df0d4b009e6d60cac2f00b2ccdb8873
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-23 21:38:30 +0800

    moved refine guess on feedback to game logic and fixed random_guess function to not repeat the same number

--- lib/mastermind/code_maker/computer_guesser.rb
+++ lib/mastermind/code_maker/computer_guesser.rb
@@ -1,55 +1,33 @@
 # frozen_string_literal: true
 
-Class ComputerGuesses
-def initialize(difficulty)
-  @difficulty = difficulty
-  @random_guesses = case difficulty
-                    when :easy then 6
-                    when :medium then 4
-                    when :hard then 2
-                    end
-  @previous_guesses = []
-end
-
-def next_guess(previous_guesses)
-  if previous_guesses.size < @random_guesses
-    random_guess
-  else
-    refine_based_on_feedback(previous_guesses.last[:feedback])
-  end
-end
-
-def random_guess
-  Array.new(4) { rand(1..6) }
-end
-
-def refined_guess(feedback)
-  if feedback.any? { |peg| %i[black white].include?(peg) }
-    refine_based_on_feedback
-  else
-    random_guess
-  end
-end
-
-def refine_based_on_feedback(feedback)
-  last_guess = @previous_guesses.last[:guess]
-  new_guess = last_guess.dup
-
-  if feedback[0..1].any? { |peg| %i[black white].include?(peg) }
-    new_guess[0] = rand(1..6).to_a.reject { |n| n == last_guess[0] }.sample unless feedback[0] == :black
-    new_guess[1] = rand(1..6).to_a.reject { |n| n == last_guess[1] }.sample unless feedback[1] == :black
+class ComputerGuesser
+  def initialize(difficulty)
+    @difficulty = difficulty
+    @random_guesses = case difficulty
+                      when :easy then 6
+                      when :medium then 4
+                      when :hard then 2
+                      end
+    @guess_history = []
   end
 
-  if feedback[2..3].any? { |peg| %i[black white].include?(peg) }
-    new_guess[2] = rand(1..6).to_a.reject { |n| n == last_guess[2] }.sample unless feedback[2] == :black
-    new_guess[3] = rand(1..6).to_a.reject { |n| n == last_guess[3] }.sample unless feedback[3] == :black
-  end
-
-  feedback.each_with_index do |peg, i|
-    if peg == :white
-      swap_index = (i + 1) % 4
-      new_guess[i], new_guess[swap_index] = new_guess[swap_index], new_guess[i]
+  def next_guess(previous_guesses)
+    if @guess_history.size < @random_guesses
+      random_guess
+    else
+      refine_based_on_feedback(previous_guesses.last[:feedback])
     end
   end
-  new_guess
+
+  def random_guess
+    (1..6).to_a.sample(4)
+  end
+
+  def refined_guess(feedback)
+    if feedback.any? { |peg| %i[black white].include?(peg) }
+      refine_based_on_feedback
+    else
+      random_guess
+    end
+  end
 end
